461d018a318da64d5151af2d8de758c36e7e7082
antonioalmeida <theantonioalmeida@gmail.com>
2024-06-09 14:55:22 +0200

    fix comments in exercises file

--- week1/databases/databases-week1-exercises.sql
+++ week1/databases/databases-week1-exercises.sql
@@ -1,46 +1,48 @@
---1. What are the names of countries with population greater than 8 million?
+-- 1. What are the names of countries with population greater than 8 million?
+SELECT Name 
+FROM country
+WHERE Population > 8000000;
+
+-- 2. What are the names of countries that have “land” in their names?
 SELECT ...
 
---2. What are the names of countries that have “land” in their names?
+-- 3. What are the names of the cities with population in between 500,000 and 1 million?
 SELECT ...
 
---3. What are the names of the cities with population in between 500,000 and 1 million?
+-- 4. What's the name of all the countries on the continent ‘Europe’?
 SELECT ...
 
---4. What's the name of all the countries on the continent ‘Europe’?
+-- 5. What are the names of all the cities in the Netherlands?
 SELECT ...
 
---5. What are the names of all the cities in the Netherlands?
+-- 6. What is the population of Rotterdam?
 SELECT ...
 
---6. What is the population of Rotterdam?
+-- 7. Which countries don't have a head of state? Hint: looks for NULL and '' values
 SELECT ...
 
---7. Which countries don't have a head of state? Hint: looks for NULL and '' values
+-- 8. What's the top 10 least populated cities? Return the name and population
 SELECT ...
 
---8. What's the top 10 least populated cities? Return the name and population
+-- 9. What countries in Africa have the local name the same as their common name?
 SELECT ...
 
---9. What countries in Africa have the local name the same as their common name?
+-- 10. What countries have Spanish as official language? Hint: see countrylanguage table
 SELECT ...
 
---10. What countries have Spanish as official language? Hint: see countrylanguage table
+-- 11. What countries have official languages spoken between 1% and 10% of the population?
 SELECT ...
 
---11. What countries have official languages spoken between 1% and 10% of the population?
+-- 12. What languages are spoken by over 90% of the population of a country? Return just the language names, but don't repeat entries
 SELECT ...
 
---12. What languages are spoken by over 90% of the population of a country? Return just the language names, but don't repeat entries
+-- 13. In which countries is 'Creole English' used? Order by descending percentage of speakers
 SELECT ...
 
---13. In which countries is 'Creole English' used? Order by descending percentage of speakers
+-- 14. What are the 5 oldest countries (by independence date) with some form of republic government? Tip: there are multiple types of republic
 SELECT ...
 
---14. What are the 5 oldest countries (by independence date) with some form of republic government? Tip: there are multiple types of republic
-SELECT ...
-
---15. For each country, how many people speak each language? Important: we want absolute values, not a percentage. Return the name of the country, the name of the language, and number of speakers of that language - Hint: you need both the country and countrylanguage tables - Hint: you can do calculations between columns, for example (SELECT a - b from table;)
+-- 15. For each country, how many people speak each language? Important: we want absolute values, not a percentage. Return the name of the country, the name of the language, and number of speakers of that language - Hint: you need both the country and countrylanguage tables - Hint: you can do calculations between columns, for example (SELECT a - b from table;)
 SELECT ...
 
 
